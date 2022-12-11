8a506633d8743c9b5139adf443124fa4a0fc0c21
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2022-12-11 22:29:50 +0000

    Create prompt for user to define number of squares per side of grid

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
 const gridContainer = document.querySelector("#gridContainer");
 
-for (let i = 0; i < 256; i++) {
-    const artGrid = document.createElement("div");
-    artGrid.setAttribute("class", "grid");
-    gridContainer.appendChild(artGrid);
+function gridSize () {
+    let widthOfGrid = prompt (`Declare grid size please`, 0);
+    timesToPrintAuto = Array(Number(widthOfGrid) + 1).join("auto ");
+    console.log(timesToPrintAuto);
+    gridContainer.setAttribute("style", `grid-template-columns: ${timesToPrintAuto};`);
+    areaOfGrid = widthOfGrid ** 2;
+    for (let i = 0; i < areaOfGrid; i++) {
+        const artGrid = document.createElement("div");
+        artGrid.setAttribute("class", "grid");
+        gridContainer.appendChild(artGrid);
+    }
 }
+
+gridSize ();
