fb42e7610dfc878b070db44eb9928549d6d34525
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2022-12-11 22:32:26 +0000

    Add explanatory comments

--- javascript.js
+++ javascript.js
@@ -1,10 +1,12 @@
 const gridContainer = document.querySelector("#gridContainer");
 
 function gridSize () {
+    //ask for grid width
     let widthOfGrid = prompt (`Declare grid size please`, 0);
+    //apply grid width info to css
     timesToPrintAuto = Array(Number(widthOfGrid) + 1).join("auto ");
-    console.log(timesToPrintAuto);
     gridContainer.setAttribute("style", `grid-template-columns: ${timesToPrintAuto};`);
+    //generate appropriate number of cells
     areaOfGrid = widthOfGrid ** 2;
     for (let i = 0; i < areaOfGrid; i++) {
         const artGrid = document.createElement("div");
