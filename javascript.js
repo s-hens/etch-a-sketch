d48ce2f6e8d6796cbde9bcffacdff2aaeaf38189
Stephanie Henson <108551327+s-hens@users.noreply.github.com>
2022-12-12 15:42:05 +0000

    Modify function so user defines number of squares per side of grid using slider instead of prompt

--- javascript.js
+++ javascript.js
@@ -1,22 +1,35 @@
 const gridContainer = document.querySelector("#gridContainer");
 
+var cellSlider = document.getElementById("cellSlider");
+
 function gridSize () {
     //ask for grid width
-    let widthOfGrid = prompt (`Declare grid size please`, 0);
+    let widthOfGrid = cellSlider.value;
     //apply grid width info to css
     timesToPrintAuto = Array(Number(widthOfGrid) + 1).join("auto ");
     gridContainer.setAttribute("style", `grid-template-columns: ${timesToPrintAuto};`);
     //generate appropriate number of cells
     areaOfGrid = widthOfGrid ** 2;
     for (let i = 0; i < areaOfGrid; i++) {
-        const artGrid = document.createElement("div");
-        artGrid.setAttribute("class", "grid");
-        gridContainer.appendChild(artGrid);
+        const gridCell = document.createElement("div");
+        gridCell.setAttribute("class", "grid");
+        gridContainer.appendChild(gridCell);
     }
 }
 
 gridSize ();
 
+//cells fill black on mouseover
 gridContainer.addEventListener("mouseover", function (e) {
     e.target.setAttribute("style", `background-color: #000000;`);
   });
+
+
+//when slider is moved, a new grid is generated accordingly
+cellSlider.oninput = function() {
+    //delete old grid
+    gridContainer.innerHTML = ``;
+    //create new grid
+    sliderOutput.innerHTML = this.value;
+    gridSize ();
+}
